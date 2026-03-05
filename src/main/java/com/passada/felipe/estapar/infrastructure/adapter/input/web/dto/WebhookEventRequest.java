2e24b75c8fd34970fab174fb98568380e1b6ca6d
fpassada <felipepassada@gmail.com>
2026-03-05 10:02:34 -0300

    fix: add `JsonFormat` to ensure application can convert the datetime received by the simulator to Instant

--- src/main/java/com/passada/felipe/estapar/infrastructure/adapter/input/web/dto/WebhookEventRequest.java
+++ src/main/java/com/passada/felipe/estapar/infrastructure/adapter/input/web/dto/WebhookEventRequest.java
@@ -1,15 +1,22 @@
 package com.passada.felipe.estapar.infrastructure.adapter.input.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 
 public record WebhookEventRequest(
         @JsonProperty("event_type") EventType type,
         @JsonProperty("license_plate") String plate,
-        @JsonProperty("entry_time") Instant entryTime,
-        @JsonProperty("exit_time") Instant exitTime,
+
+        @JsonProperty("entry_time")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
+        Instant entryTime,
+
+        @JsonProperty("exit_time")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "UTC")
+        Instant exitTime,
+
         Double lat,
         Double lng
 ) {
